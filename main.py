63aaa888e36ad1490aeeecab3d9fbced982bddff
MuhRagilSalsabil <143258887+MuhRagilSalsabil@users.noreply.github.com>
2024-10-21 18:46:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -30,10 +30,10 @@ def cartoonize_image(model, image):
 # Streamlit Interface
 st.title("KARTUNISASI GAMBAR BERTEMA SENI BUDAYA MADURA dengan CARTOON GENERATIVE ADVERSARIAL NETWORK (CartoonGAN)")
 
-tab1, tab2 = st.tabs(["Pengenalan", "Implementasi"])
+tab1, tab2 = st.tabs(["Halaman Utama", "Implementasi"])
 with tab1:
-    st.header('CartoonGAN')
-    st.text('Cartoon Generative Adversarial Network (CartoonGAN) merupakan')
+    st.header('Pengenalan')
+    st.text('Kartun merupakan salah satu bentuk seni yang sering kita jumpai dalam kehidupan sehari-hari. Selain nilai seninya yang tinggi, kartun juga memiliki kegunaan yang beragam, mulai dari tampilan cetak hingga digunakan sebagai alat bercerita dalam pendidikan anak.')
 with tab2:
     # Input gambar
     uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
